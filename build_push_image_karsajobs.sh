7046eec69d65f1cda0c92c28a6562fd913ba7e05
caturheri <caturheriprayitno@gmail.com>
2023-12-23 14:39:30 +0700

    build and push image

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -7,7 +7,7 @@ docker build -t ghcr.io/caturheri/karsajobs:latest
 docker images
 
 # Login ke Github Packages via Terminal
-echo $CR_PAT | docker login ghcr.io -u caturheri --pasword-stdin
+echo $CR_PAT | docker login ghcr.io -u caturheri --password-stdin
 
 # Mengunggah image ke Github Packages
 docker push ghcr.io/caturheri/karsajobs:latest
