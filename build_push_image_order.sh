54dfb75f09ce66af111f882eaebc28164ab6ac04
caturheri <caturheriprayitno@gmail.com>
2023-12-30 22:51:07 +0700

    submission sukses

--- build_push_image_order.sh
+++ build_push_image_order.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 
-# Nama Docker image untuk backend
-IMAGE_NAME="widiarrohman/order-service:latest"
+# Perintah untuk membuat Docker image dari Dockerfile
+docker build -t ghcr.io/caturheri/order-service:latest .
 
-# Build Docker image dari Dockerfile
-docker build -t $IMAGE_NAME .
+# Melihat daftar image di lokal
+docker images
 
-# jalankan dulu di terminal
-# export PASSWORD_DOCKER_HUB=<password_Anda>
+#  Login ke Github Packages via Terminal
+echo $CR_PAT | docker login ghcr.io -u caturheri --password-stdin
 
-# Login ke Docker Hub dengan menggunakan environment variable
-echo $PASSWORD_DOCKER_HUB | docker login -u widiarrohman --password-stdin
-
-# tag image dengan nama widiarrohman/order-service:latest
-docker tag order-service:latest widiarrohman/order-service:latest
-
-# Push image ke Docker Hub
-docker push $IMAGE_NAME
-
-# Logout dari Docker Hub
-docker logout
-
-# cara menjalankan file build_push_image_order.sh
-# chmod +x build_push_image_order.sh
-# ./build_push_image_order.sh
-
-# cara membuat container dari image ini
-# docker run -d -p 8080:8080 widiarrohman/order-service:latest
+# Mengunggah image ke Github Packages
+docker push ghcr.io/caturheri/order-service:latest
